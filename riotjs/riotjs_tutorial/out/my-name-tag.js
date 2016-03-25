d586ddde8b3e16bc178df22e2cfeeae2da6501b3
Jaeng Lee <jylee@innospark.com>
2016-03-25 17:24:05 +0900

    riot.js tutorial 2

---
+++ riotjs/riotjs_tutorial/out/my-name-tag.js
@@ -0,0 +1,6 @@
+riot.tag('my-name', '<h1>My Name is : {text}</h1>', function(opts){
+    this.text = 'Jaeng'
+});
+riot.tag('my-age', '<h1>My age is : {text}</h1>', function(opts){
+    this.text = 28
+});
